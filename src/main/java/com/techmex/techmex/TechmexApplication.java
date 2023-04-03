85f0344177bfde83af923247f639ce625415349d
Tatiiics <tatiianacs@gmail.com>
2023-04-03 11:07:08 +0200

    creacion proyecto

---
+++ src/main/java/com/techmex/techmex/TechmexApplication.java
@@ -0,0 +1,13 @@
+package com.techmex.techmex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techmex/techmex/TechmexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmex.techmex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
