d06ab80c7c87d79ed2cf37a897a4eb0ac62dc2c9
DESKTOP-UU9M14G\Gonsalito <gonzalodelcodoserranoo@gmail.com>
2023-04-21 00:30:14 +0200

    Crear interfacesDAO

---
+++ src/main/java/com/techmex/techmex/Data/Entities/CategoriasModel.java
@@ -0,0 +1,23 @@
+package com.techmex.techmex.Data.Entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Table (name = "Categorias")
+
+public class CategoriasModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_categorias;
+
+
+}
